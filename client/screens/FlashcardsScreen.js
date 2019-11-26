0597e5e054207ba648888cdeea9838621aaed436
Noor <noor_khan@alumni.ubc.ca>
2019-11-25 23:18:34 -0800

    flashcards screen done

--- client/screens/FlashcardsScreen.js
+++ client/screens/FlashcardsScreen.js
@@ -1,34 +1,66 @@
-import React, { Component } from 'react';
-import {
-  Image,
-  Platform,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-  Button
-} from 'react-native';
-import colors from '../assets/colors';
+import React, { Component } from "react";
+import { ScrollView, View, SafeAreaView, StyleSheet } from "react-native";
+import colors from "../assets/colors";
+import Card from "../components/Card";
 
 export default class FlashcardsScreen extends Component {
-  
   static navigationOptions = {
-    title: 'Flashcards',
+    title: "Flashcards",
     headerStyle: {
       backgroundColor: colors.primary
     },
     headerTintColor: colors.primaryText,
     headerTitleStyle: {
-        fontWeight: 'bold',
-    },
+      fontWeight: "bold"
+    }
   };
 
   render() {
     return (
-      <View>
-
-      </View>
+      <ScrollView style={styles.container}>
+        <SafeAreaView style={styles.wrapper}>
+          <View style={styles.content}>
+            <ScrollView
+              style={styles.container}
+              contentContainerStyle={styles.contentContainer}
+            >
+              <Card
+                uri="https://static2.bigstockphoto.com/8/5/1/large1500/158296634.jpg"
+                cardTitle="Respiratory System"
+                callback={() => this.props.navigation.navigate("Flash")}
+              />
+              <Card
+                uri="https://st.depositphotos.com/2363887/2564/i/950/depositphotos_25640047-stock-photo-man-anatomy-thorax-cutaway-with.jpg"
+                cardTitle="Thorax"
+                callback={() => this.props.navigation.navigate("Flash")}
+              />
+              <Card
+                uri="https://www.simtics.com/media/28745/mlla.jpg"
+                cardTitle="Lower Limb"
+                callback={() => this.props.navigation.navigate("Flash")}
+              />
+              <Card
+                uri="http://www.interactive-biology.com/wp-content/uploads/2012/10/Hand-and-arm-bone-1280x640.jpg"
+                cardTitle="Upper Limb"
+                callback={() => this.props.navigation.navigate("Quiz")}
+              />
+              <Card
+                uri="https://c1.wallpaperflare.com/preview/661/540/52/skeleton-hand-bones-anatomy.jpg"
+                cardTitle="The Metacarpals"
+                callback={() => this.props.navigation.navigate("Quiz")}
+              />
+            </ScrollView>
+          </View>
+        </SafeAreaView>
+      </ScrollView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 15,
+    backgroundColor: "#fff"
+  }
+});
--- client/screens/QuizzesScreen.js
+++ client/screens/QuizzesScreen.js
@@ -1,24 +1,59 @@
-import React, { Component } from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import colors from '../assets/colors';
+import React, { Component } from "react";
+import { ScrollView, StyleSheet, SafeAreaView, View } from "react-native";
+import Card from "../components/Card";
+import colors from "../assets/colors";
 
 export default class Quizzes extends Component {
-
   static navigationOptions = {
-    title: 'Quizzes',
+    title: "Quizzes",
     headerStyle: {
       backgroundColor: colors.primary
     },
     headerTintColor: colors.primaryText,
     headerTitleStyle: {
-        fontWeight: 'bold',
-    },
+      fontWeight: "bold"
+    }
   };
 
   render() {
     return (
       <ScrollView style={styles.container}>
-  
+        <SafeAreaView style={styles.wrapper}>
+          <View style={styles.content}>
+            <ScrollView
+              style={styles.container}
+              contentContainerStyle={styles.contentContainer}
+            >
+              <Card
+                style={styles.card}
+                uri="http://www.aljanh.net/data/archive/img/3085128125.jpeg"
+                cardTitle="Heart"
+                callback={() => this.props.navigation.navigate("Quiz")}
+              />
+              <Card
+                style={styles.card}
+                uri="https://cayugaent.com/wp-content/uploads/2016/01/Cayuga-ENT-HeadandNeckCancer.jpg"
+                cardTitle="Head & Neck"
+                callback={() => this.props.navigation.navigate("Quiz")}
+              />
+              <Card
+                uri="https://www.simtics.com/media/28745/mlla.jpg"
+                cardTitle="Lower Libm"
+                callback={() => this.props.navigation.navigate("Flash")}
+              />
+              <Card
+                uri="http://www.interactive-biology.com/wp-content/uploads/2012/10/Hand-and-arm-bone-1280x640.jpg"
+                cardTitle="Upper Limb"
+                callback={() => this.props.navigation.navigate("Quiz")}
+              />
+              <Card
+                uri="https://c1.wallpaperflare.com/preview/661/540/52/skeleton-hand-bones-anatomy.jpg"
+                cardTitle="The Metacarpals"
+                callback={() => this.props.navigation.navigate("Quiz")}
+              />
+            </ScrollView>
+          </View>
+        </SafeAreaView>
       </ScrollView>
     );
   }
@@ -28,6 +63,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 15,
-    backgroundColor: '#fff',
-  },
+    backgroundColor: "#fff"
+  }
 });
