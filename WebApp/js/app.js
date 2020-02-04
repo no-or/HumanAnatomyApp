1413cad8d8b1b43464e0ea4507435dacc8b12f53
Kmarti1 <kjm.ducks@gmail.com>
2020-02-03 23:01:05 -0800

    side menu done

--- WebApp/js/app.js
+++ WebApp/js/app.js
@@ -1,48 +1,91 @@
 $(document).ready(function(){
   $("#head").click(function(){
-  	$(".subregion").remove();
-  	$(".selected").removeClass("selected");
-  	$("#head").addClass("selected");
-  	for(item in head){
-  		$("#head").after('<li id="' + head[item].replace(/\s/g, '') + '" class="subregion">')
-  		$('#' + head[item].replace(/\s/g, '')).prepend('<h3 class="category-title">' + head[item] + '</h3>')
-  	}
+  	menuChange(head, "head")
   })
   $("#lower").click(function(){
-  	$(".subregion").remove();
-  	$(".selected").removeClass("selected");
-  	$("#lower").addClass("selected");
-  	for(item in lower){
-  		$("#lower").after('<li id="' + lower[item].replace(/\s/g, '') + '" class="subregion">')
-  		$('#' + lower[item].replace(/\s/g, '')).prepend('<h3 class="category-title">' + lower[item] + '</h3>')
-  	}
+  	menuChange(lower, "lower")
   })
   $("#upper").click(function(){
-  	$(".subregion").remove();
-  	$(".selected").removeClass("selected");
-  	$("#upper").addClass("selected");
-  	for(item in upper){
-  		$("#upper").after('<li id="' + upper[item].replace(/\s/g, '') + '" class="subregion">')
-  		$('#' + upper[item].replace(/\s/g, '')).prepend('<h3 class="category-title">' + upper[item] + '</h3>')
-  	}
+  	menuChange(upper, "upper")
   })
   $("#trunk").click(function(){
-  	$(".subregion").remove();
-  	$(".selected").removeClass("selected");
-  	$("#trunk").addClass("selected");
-  	for(item in trunk){
-  		$("#trunk").after('<li id="' + trunk[item].label.replace(/\s/g, '') + '" class="subregion">')
-  		$('#' + trunk[item].label.replace(/\s/g, '')).prepend('<h3 class="category-title">' + trunk[item].label + '</h3>')
-  	}
+  	menuChange(trunk, "trunk")
   })
 });
 
-var Back = {label: "Back", subregions : ["Trunk bones", "Trunk joints"]}
-var Thorax = {label: "Thorax", subregions : ["Pectoral region", "Lungs & pleura", "Heart", "Superior and posterior mediastinum"]}
-var Abdomen = {label: "Abdomen", subregions : ["Anterior abdominal wall", "Foregut organs", "Midgut & hindgut organs", "Posterior abdominal wall"]}
-var Pelvis = {label: "Pelvis", subregions: ["Pelvic bones and muscles", "Pelvic joints & ligaments", "Male pelvis", "Female pelvis", "Perineum"]}
+function menuChange(region, regionName) {
+  $(".subRegion").remove();
+  $(".subSubRegion").remove();
+  var curSelected = document.getElementsByClassName("regionSelected");
+  if(!(curSelected.length == 1 && curSelected[0].id == regionName)){
+    $(".regionSelected").removeClass("regionSelected");
+    addMenuItems(region, regionName, "region", function(thisElement){
+      $(".subRegionSelected").removeClass("subRegionSelected")
+      $(thisElement).addClass("subRegionSelected")
+    });
+  } else {
+    $(".regionSelected").removeClass("regionSelected");
+  }
+}
 
-var trunk = [Back, Thorax, Abdomen, Pelvis];
+function subMenuChange(region, regionName) {
+  $(".subRegionSelected").removeClass("subRegionSelected")
+  var curSelected = document.getElementsByClassName("subSubRegionSelected");
+  if(!(curSelected.length == 1 && curSelected[0].id == regionName)){
+    addMenuItems(region, regionName, "subRegion" , function(thisElement){
+      $(".subSubRegionSelected").removeClass("subSubRegionSelected")
+      $(thisElement).addClass("subSubRegionSelected")
+    })   
+  }else {
+    $(".subSubRegionSelected").removeClass("subSubRegionSelected")
+  }
+}
+
+function addMenuItems(region, regionName, regionLevel, onClick){
+  $(".subSubRegion").remove();
+  $("#" + regionName).addClass(regionLevel + "Selected");
+  for(item in region){
+    if(regionLevel == "region"){
+      $("#" + regionName).after('<li id="' + region[item].replace(/\s/g, '') + '" class="subRegion"> ')
+      $('#' + region[item].replace(/\s/g, '')).prepend('<h5 class="category-title">' + region[item] + '</h5>')
+    } else{
+      $("#" + regionName).after('<li id="' + region[item].replace(/\s/g, '') + '" class="subSubRegion"> ')
+      $('#' + region[item].replace(/\s/g, '')).prepend('<h6 class="category-title">' + region[item] + '</h6>')
+    }
+    if(regionName != "trunk"){
+      $('#' + region[item].replace(/\s/g, '')).click(function(){
+        onClick(this);
+      })
+    }else {
+      trunkSetup();
+    }
+  }
+}
+
+function trunkSetup() {
+  $("#Back").click(function() {
+    subMenuChange(Back, "Back");
+  })
+  $("#Thorax").click(function() {
+    subMenuChange(Thorax, "Thorax");
+  })
+  $("#Abdomen").click(function() {
+    subMenuChange(Abdomen, "Abdomen");
+  })
+  $("#Pelvis").click(function() {
+    subMenuChange(Pelvis, "Pelvis");
+
+  })
+}
+
+
+var Back = ["Trunk bones", "Trunk joints"];
+var Thorax = ["Pectoral region", "Lungs and pleura", "Heart", "Superior and posterior mediastinum"]
+var Abdomen = ["Anterior abdominal wall", "Foregut organs", "Midgut and hindgut organs", "Posterior abdominal wall"]
+var Pelvis = ["Pelvic bones and muscles", "Pelvic joints and ligaments", "Male pelvis", "Female pelvis", "Perineum"]
+
+var trunk = ["Back", "Thorax", "Abdomen", "Pelvis"];
 var lower = ["Gluteal region", "Thigh", "Leg", "Foot", "Lower limb bones", "Lower limb joints"]
 var upper = ["Scapular region", "Axilla", "Arm", "Forearm", "Hand", "Upper limb bones", "Upper limb Joints"];
 var head = ["Skull and meninges", "Cavernous Sinus", "Orbit", "Neck", "Face", "Infratemporal fossa", "Oral Cavity", "Pharynx", "Larynx"]
+
