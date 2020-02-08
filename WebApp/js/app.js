12f100fac929bdef16c5ec699300affc8cc39dc7
Kmarti1 <kjm.ducks@gmail.com>
2020-02-07 19:29:46 -0800

    better web

--- WebApp/js/app.js
+++ WebApp/js/app.js
@@ -13,60 +13,7 @@ $(document).ready(function(){
   $("#trunk").click(function(){
   	menuChange(trunk, "trunk")
   })
-  $("#login-form").submit(function( event ) {
-    userLogin(event, $(this).serializeArray())
-  });
-  $("#code-form").submit(function( event ) {
-    codeToSignup(event, $(this).serializeArray())
-  });
-  $("#signup-form").submit(function( event ) {
-    userSignup(event, $(this).serializeArray())
-  });
 });
-
-function userSignup(event, values) {
-  var data = {
-    name: values[0].value,
-    email: values[1].value,
-    password: values[2].value,
-  }
-  var url = 'http://localhost:8080/admin/register'
-  var onSuccess = function() {
-    alert("Successful User Creation")
-    window.location.href = 'login.html';
-  }
-  var onFailure = function (response) {alert(response)};
-  ajaxPost(url, data, onSuccess, onFailure);
-  event.preventDefault();
-}
-
-function userLogin(event, values) {
-    var data = {
-      email: values[0].value,
-      password: values[1].value
-    }
-    var url = 'http://localhost:8080/admin/login'
-    var onSuccess = function() {window.location.href = 'index.html';}
-    var onFailure = function (response) {alert(response)};
-    ajaxPost(url, data, onSuccess, onFailure);
-    event.preventDefault();
-}
-
-function codeToSignup(event, values) {
-  var url = "http://localhost:8080/code";
-  var onSuccess = function(response) {
-    var inputCode = values[0].value;
-    var dbCode = response[0].code;
-    if (inputCode == dbCode) {
-      window.location.href = 'signup.html';
-    } else {
-      alert("Invalid Secret Code")
-    }
-  }
-  var onFailure = function (response) {alert(response)};
-  ajaxGet(url, onSuccess, onFailure);
-  event.preventDefault();
-}
 
 function menuChange(region, regionName) {
   $(".subRegion").remove();
@@ -86,15 +33,17 @@ function menuChange(region, regionName) {
 }
 
 function subMenuChange(region, regionName) {
-  $(".subRegionSelected").removeClass("subRegionSelected")
-  var curSelected = document.getElementsByClassName("subSubRegionSelected");
+  var curSelected = document.getElementsByClassName("subRegionSelected");
   if(!(curSelected.length == 1 && curSelected[0].id == regionName)){
+    $(".subRegionSelected").removeClass("subRegionSelected")
     addMenuItems(region, regionName, "subRegion" , function(thisElement){
       $(".subSubRegionSelected").removeClass("subSubRegionSelected")
       $(thisElement).addClass("subSubRegionSelected")
     })
   }else {
     $(".subSubRegionSelected").removeClass("subSubRegionSelected")
+    $(".subSubRegion").remove();
+    $(".subRegionSelected").removeClass("subRegionSelected")
   }
 }
 
@@ -341,90 +290,6 @@ function deleteQuiz(){
     })
   }
 }
-
-var ajaxDelete = function (url, onSuccess, onError){
-  this.url = url;
-  this.onSuccess = onSuccess;
-  this.onError = onError;
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-        if(xhttp.readyState == 4 && xhttp.status == 200) {
-            onSuccess(xhttp.responseText);            
-        }
-        //else onError(xhttp.responseText);
-    }
-    xhttp.open('DELETE', this.url, true);
-    xhttp.setRequestHeader('Auth-token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZTMxMGMyZjUzZmZjMjFhMGEzYjNlZmYiLCJpYXQiOjE1ODAyNzc2OTl9.XZnj5CtA_rYOxDo48d_kK3l4_EkEvf91ZoeG4-0naXA');
-    xhttp.setRequestHeader('Content-Type', "application/json")
-    xhttp.send();
-}
-
-var ajaxPost = function (url, data, onSuccess, onError){
-    this.url = url;
-    this.onSuccess = onSuccess;
-    this.onError = onError;
-
-    var xhttp = new XMLHttpRequest();
-    //xhttp.timeout = 5000;
-    var i = 0;
-
-    xhttp.onreadystatechange = function() {
-        if(xhttp.readyState == 4 && xhttp.status == 200) {
-            onSuccess(xhttp.responseText);
-        }
-        else {
-          if (xhttp.readyState == 4) {
-            onError(xhttp.responseText);
-          }
-        }
-    }
-    xhttp.open('POST', this.url, true);
-    xhttp.setRequestHeader('Auth-token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZTMxMGMyZjUzZmZjMjFhMGEzYjNlZmYiLCJpYXQiOjE1ODAyNzc2OTl9.XZnj5CtA_rYOxDo48d_kK3l4_EkEvf91ZoeG4-0naXA');
-    xhttp.setRequestHeader('Content-Type', "application/json")
-    xhttp.send(JSON.stringify(data));
-
-}
-
-var ajaxGet = function (url, onSuccess, onError) {
-    this.url = url;
-    this.onSuccess = onSuccess;
-    this.onError = onError;
-    var i = 0
-
-    var xhttp = new XMLHttpRequest();
-    xhttp.timeout = 5000;
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-            console.log("response: ");
-            console.log(typeof(JSON.parse(xhttp.responseText)));
-            onSuccess(JSON.parse(xhttp.responseText));
-        }
-        else {
-            if (i > 2) {
-                console.log("error: ");
-                console.log(xhttp.responseText);
-                if(this.status == 404){
-                  var obj = [];
-                  onSuccess(obj);
-                }else{
-                  onError(xhttp.responseText);
-                }
-                i = 0;
-            }else{
-              i++;
-            }
-        }
-    }
-    xhttp.ontimeout = function (e) {
-      alert("fail")
-        console.log("error: ");
-        console.log("timeout");
-        onError(xhttp.responseText);
-    }
-    xhttp.open("GET", url, true);
-    xhttp.send();
-}
-
 
 
 
