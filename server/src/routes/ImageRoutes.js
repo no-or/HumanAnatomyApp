6ad6896b93054a61d73b5b24676931e719f82304
Noor <noor_khan@alumni.ubc.ca>
2020-02-19 19:49:52 -0800

    S3 service and Post Image

--- server/src/routes/ImageRoutes.js
+++ server/src/routes/ImageRoutes.js
@@ -1,33 +1,20 @@
 const express = require("express");
-const ImageModel = require("../models/images");
+const upload = require("../services/imgUpload");
+
+const singleUpload = upload.single('image');
 
 const initializeImageRoutes = (app) => {
     const imageRouter = express.Router();
     app.use('/image', imageRouter);
 
     /* create an image */
-    imageRouter.post('/', async (req, res) => {
-        const question = new ImageModel(req.body);
-        try {
-            await question.save().then((item) => res.send(item));
-        } catch (e) {
-            console.error(e);
-            res.status(500);
-            res.json('Could not create the Image');
-        }
-    });
-
-    /* get an image with id */
-    imageRouter.get('/:id', async (req, res) => {
-        try {
-            const question = await ImageModel.findById(req.params.id);
-            res.status(200);
-            res.json(question);
-        } catch (e) {
-            console.error(e);
-            res.status(500);
-            res.json('Could not fetch the Image');
-        }
+    imageRouter.post('/', (req, res) => {
+        singleUpload(req, res, function(err) {
+            if (err) {
+                return res.status(422).send({errors: [{title: 'File Upload Error', detail: err.message}] });
+            }
+            return res.json({'imageUrl': req.file.location});
+        });
     });
 };
 
