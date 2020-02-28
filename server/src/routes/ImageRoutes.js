038f4363b7dde979d06ab9e92517d94d8a7107a6
Noor <noor_khan@alumni.ubc.ca>
2020-02-27 21:21:01 -0800

    added cludfront route

--- server/src/routes/ImageRoutes.js
+++ server/src/routes/ImageRoutes.js
@@ -2,6 +2,9 @@ const express = require("express");
 const upload = require("../services/imgUpload");
 const aws = require('aws-sdk');
 const s3 = new aws.S3();
+const verifyAdmin = require("../util/verifyToken");
+
+const CLOUDFRONT = 'd1tjf70w3r56al.cloudfront.net';
 
 aws.config.update({
     secretAccessKey: process.env.AWS_USER_SECRET_KEY,
@@ -16,17 +19,17 @@ const initializeImageRoutes = (app) => {
     app.use('/image', imageRouter);
 
     /* Post an image to AWS S3 */
-    imageRouter.post('/', (req, res) => {
+    imageRouter.post('/', verifyAdmin, (req, res) => {
         singleUpload(req, res, function(err) {
             if (err) {
                 return res.status(422).send({errors: [{title: 'File Upload Error', detail: err.message}] });
             }
-            return res.json({'imageUrl': req.file.location});
+            return res.json({'imageUrl': `${CLOUDFRONT}/${req.file.key}`});
         });
     });
 
     /* Remove an image from AWS S3 */
-    imageRouter.delete('/:id', (req, res) => {
+    imageRouter.delete('/:id', verifyAdmin, (req, res) => {
         const params = {
             Bucket: 'anatomy-bucket',
             Key: req.params.id,
