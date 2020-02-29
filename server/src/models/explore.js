6afab43f2d3592324b3ce36efc1a790e6a1008d7
Noor <noor_khan@alumni.ubc.ca>
2020-02-28 16:51:55 -0800

    title must be unique

--- server/src/models/explore.js
+++ server/src/models/explore.js
@@ -7,7 +7,8 @@ const ExplorelabSchema = new mongoose.Schema({
     },
     title:{
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     imageUrl:{
         type: String,
