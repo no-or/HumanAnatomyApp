84fc4c47d09a3ebc4a8bad9164e1aac7fcb823a5
Adrian Viquez <aviquez96@gmail.com>
2020-03-16 09:12:23 -0700

    WIP

--- client/screens/WelcomeScreen.js
+++ client/screens/WelcomeScreen.js
@@ -6,13 +6,34 @@ import {
   SafeAreaView,
   View,
   Platform,
-  TouchableOpacity
+  TextInput,
+  TouchableOpacity,
+  Keyboard,
+  TouchableWithoutFeedback
 } from "react-native";
-import colors from "../assets/colors";
-import { InputStyled } from "../components/InputStyled";
+import colors from "../assets/colors"
 import { ButtonStyled } from "../components/ButtonStyled";
+import { HOST_NAME } from "../constants/Constants";
+
+const DismissKeyboard = ({children}) => (
+  <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+    {children}
+  </TouchableWithoutFeedback>
+)
 
 export default class WelcomeScreen extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      university: null,
+      degree: null,
+      educationLevel: null,
+      year: null,
+     }
+  }
+
+
   static navigationOptions = {
     title: "Welcome",
     headerStyle: {
@@ -24,28 +45,79 @@ export default class WelcomeScreen extends Component {
     }
   };
 
+  postStats = () => {
+    const host = HOST_NAME
+    fetch(HOST_NAME + "/stat", {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        university: this.state.university,
+        degree: this.state.degree,
+        educationLevel: this.state.educationLevel,
+        year: parseInt(this.state.year),
+      }),
+    }).then((response) => response.json())
+      .then((responseJson) => {
+        return responseJson;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  navigateRoot = () => {
+    this.postStats()
+    this.props.navigation.navigate("Root")
+  }
+
   render() {
     return (
       <SafeAreaView style={styles.wrapper}>
-        <View style={styles.content}>
-          <Text style={styles.heading}>WELCOME</Text>
-          <InputStyled placeholder="     What is your university?" />
-          <InputStyled placeholder="     What is your degree?" />
-          <InputStyled placeholder="     What is your degree level?" />
-          <InputStyled placeholder="     What is your current year?" />
-          {/* // TODO why do we need this information? */}
-          <ButtonStyled
-            text="Continue"
-            onPress={() => this.props.navigation.navigate("Root")}
-          />
-          <TouchableOpacity>
-            <Text style={styles.text}>Not A Student?</Text>
-          </TouchableOpacity>
-          {/* <Button
-            title={"Not a Student?"}
-            // onPress={() => this.props.navigation.navigate("Root")}
-          /> */}
-        </View>
+        <DismissKeyboard>
+          <View style={styles.content}>
+            <Text style={styles.heading}>WELCOME</Text>
+            <TextInput
+              style={styles.input}
+              autoCapitalize="words"
+              autoCorrect={true}
+              placeholder="What is your university?"
+              onChangeText={(text) => this.setState({university: text})}
+            />
+            <TextInput
+              style={styles.input}
+              autoCapitalize="words"
+              autoCorrect={true}
+              placeholder="What is your degree?"
+              onChangeText={(text) => this.setState({degree: text})}
+            />
+            <TextInput
+              style={styles.input}
+              autoCapitalize="words"
+              autoCorrect={true}
+              placeholder="What is your educational level?"
+              onChangeText={(text) => this.setState({educationLevel: text})}
+            />
+            <TextInput
+              style={styles.input}
+              autoCapitalize="words"
+              autoCorrect={true}
+              placeholder="What is your current year?"
+              keyboardType="number-pad"
+              onChangeText={(text) => this.setState({year: text})}
+            />
+            <ButtonStyled
+              text="Continue"
+              onPress={this.navigateRoot}
+            />
+            <TouchableOpacity>
+              <Text style={styles.text}>Not A Student?</Text>
+            </TouchableOpacity>
+
+          </View>
+        </DismissKeyboard>
       </SafeAreaView>
     );
   }
@@ -71,14 +143,22 @@ const styles = StyleSheet.create({
     width: "100%",
     padding: 32
   },
-  // text: {
-  //   alignSelf: "center",
-  //   textAlign: "center"
-  // },
   text: {
     color: "#FFF",
     textAlign: "center",
     fontWeight: "bold",
     fontSize: 16,
+  },
+  input: {
+    backgroundColor: "white",
+    borderRadius: 24,
+    height: 55,
+    width: "100%",
+    margin: 15,
+    textAlign: "center",
+    color: "#051d63",
+    fontWeight: "500",
+    fontFamily: Platform.OS === "ios" ? "Helvetica" : "Roboto",
+    fontSize: 15
   }
 });
