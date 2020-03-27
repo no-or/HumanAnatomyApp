77b352179d1187951f9b3f03169b840b1317a902
Adrian Viquez <aviquez96@gmail.com>
2020-03-27 15:36:44 -0700

    WIP Explore Bugs

--- client/screens/ExploreLabLearnDropdownOptionScreen.js
+++ client/screens/ExploreLabLearnDropdownOptionScreen.js
@@ -4,15 +4,15 @@ import {
   StyleSheet,
   SafeAreaView,
   View,
-  Text, 
+  Text,
   FlatList,
-  SectionList,
   ScrollView,
   Platform
 } from 'react-native';
 import colors from '../assets/colors';
 import TabBarIcon from "../components/TabBarIcon";
 import {HOST_NAME} from "../constants/Constants"
+import ReactNativeZoomableView from '@dudigital/react-native-zoomable-view/src/ReactNativeZoomableView';
 
 import offline from "../Offline";
 
@@ -20,12 +20,26 @@ function Item({ content }) {
 
     return (
         <View style={styles.optionContainer}>
-            <ScrollView minimumZoomScale={1} maximumZoomScale={5}>
+            <View style={styles.imageContainer}>
+              {/* <ReactNativeZoomableView
+                  maxZoom={2.5}
+                  minZoom={1.0}
+                  zoomStep={0.1}
+                  initialZoom={1.0}
+                  style={styles.zoomableView}
+                >
                 <Image
-                    style={styles.image}
-                    source={{uri: content.imageUrl}}
+                  style={styles.image}
+                  source={{uri: content.imageUrl}}
                 />
-            </ScrollView>
+              </ReactNativeZoomableView> */}
+              <ScrollView minimumZoomScale={1} maximumZoomScale={5} scrollEnabled={true}>
+                <Image
+                  style={styles.image}
+                  source={{uri: content.imageUrl}}
+                />
+              </ScrollView>
+            </View>
             <Text style={styles.text}>{content.title}</Text>
         </View>
     );
@@ -44,38 +58,27 @@ export default class ExploreLabLearnDropdownOptionScreen extends Component {
 
       this.off = new offline;
 
-      //These nested promises are meant to see if online or offline mode should be used.
       let promise = new Promise((resolve, reject) => {
         resolve(this.off._retrieveData('explore'));
       })
 
-      //depending whether the offline button is toggled on or off, fetch from local or remote, respectively.
       promise.then((data) => {
 
         this.setState({offline: data[this.state.region]});
 
         if(this.state.offline){
           let promise2 = new Promise((resolve, reject) => {
-            // We call resolve(...) when what we were doing asynchronously was successful, and reject(...) when it failed.
               resolve(this.off.grabData(this.state.region, 'explore'));
           })
 
           promise2.then((data) => {
-            // successMessage is whatever we passed in the resolve(...) function above.
-            console.log("offline data");
             this.setState({menu: data});
           });
         }else{
-          //else if offline not available
-          console.log("online data");
           this.apiFetch();
         }
 
       });
-    }
-
-    componentDidMount() {
-      // this.apiFetch();
     }
 
     apiFetch() {
@@ -84,7 +87,6 @@ export default class ExploreLabLearnDropdownOptionScreen extends Component {
       return fetch(host+'/explore?region=' + navigation.state.params.title)
       .then((response) => 
       response.status == 404 ? "" : response.json()
-      // alert(JSON.stringify(response))
       )
       .then((responseJson) => {
         if(responseJson != "") {
@@ -125,29 +127,15 @@ export default class ExploreLabLearnDropdownOptionScreen extends Component {
     render() {
       return (
           <SafeAreaView style={styles.container}>
+            <ScrollView>
               <FlatList
                   data={this.state.menu}
                   renderItem={({ item }) => <Item content={item}/>}
                   keyExtractor={item => item._id}
               >
               </FlatList>
+            </ScrollView>
           </SafeAreaView>
-          // <SafeAreaView style={styles.container}>
-          //      {/* <FlatList
-          //         data={this.state.data}
-          //         renderItem={({ item }) => <Item url={item.image} title={item.title}/>}
-          //         keyExtractor={item => item._id}
-          //     >
-          //     </FlatList> */}
-          //     <SectionList
-          //       sections={this.state.menu}
-          //       keyExtractor={(item, index) => item + index}
-          //       renderItem={({ item }) => <Item url={item.image} title={item.title}/>}
-          //       renderSectionHeader={({ section: { title } }) => (
-          //         <Text style={styles.header}>{title}</Text>
-          //       )}
-          //     />
-          // </SafeAreaView>
       );
     }
 }
@@ -156,10 +144,18 @@ const styles = StyleSheet.create({
     image: {
         width: "100%",
         height: 350,
-        resizeMode: "cover",
-        borderWidth: 1,
-        borderRadius: 10,
-        marginBottom: 10
+        resizeMode: "contain",
+    },
+    // zoomableView: {
+    //   alignContent: "center",
+    //   alignItems: 'center',
+    // },
+    imageContainer: {
+      flex: 3,
+      overflow: "hidden",
+      borderWidth: 1,
+      borderRadius: 10,
+      marginBottom: 10,
     },
     container: {
         justifyContent: "center",
